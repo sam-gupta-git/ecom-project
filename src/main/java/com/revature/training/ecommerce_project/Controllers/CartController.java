dcfdb441496ea08745f78990e239d59168f0915b
Aalok Zimmerman <azimmerman1245@gmail.com>
2025-10-08 13:41:58 -0500

    Created a CartController class for handling REST calls

---
+++ src/main/java/com/revature/training/ecommerce_project/Controllers/CartController.java
@@ -0,0 +1,32 @@
+package com.revature.training.ecommerce_project.Controllers;
+
+public class CartController {
+    
+    public void addToCart(String req, String res) {
+        
+    }
+
+    public void removeFromCart(String req, String res) {
+        
+    }
+
+    public void updateItemQuantity(String req, String res) {
+        
+    }
+
+    public void viewCart(String req, String res) {
+        
+    }
+
+    public void clearCart(String req, String res) {
+        
+    }
+
+    public void applyDiscountCode(String req, String res) {
+        
+    }
+
+    public void removeDiscountCode(String req, String res) {
+        
+    }
+}
