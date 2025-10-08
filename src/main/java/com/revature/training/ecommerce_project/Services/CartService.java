70221940ef14e29167a7a4ef13b17164816945f1
Aalok Zimmerman <azimmerman1245@gmail.com>
2025-10-08 13:09:22 -0500

    Establishing methods for CartService class

--- src/main/java/com/revature/training/ecommerce_project/Services/CartService.java
+++ src/main/java/com/revature/training/ecommerce_project/Services/CartService.java
@@ -1,5 +1,24 @@
 package com.revature.training.ecommerce_project.Services;
 
 public class CartService {
-    
+
+    public void addToCart(int itemID, int quantity) {
+        
+    }
+
+    public void removeFromCart(int itemID) {
+        
+    }
+
+    public void updateItemQuantity(int itemID, int newQuantity) {
+        
+    }
+
+    public void viewCart() {
+        
+    }
+
+    public void clearCart() {
+        
+    }
 }
