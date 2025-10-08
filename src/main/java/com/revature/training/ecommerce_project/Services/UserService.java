845704e4549c584d53950258fa3c5f976609e30f
Aalok Zimmerman <azimmerman1245@gmail.com>
2025-10-08 14:21:39 -0500

    Created UserController and updated UserService to have empty methods

--- src/main/java/com/revature/training/ecommerce_project/Services/UserService.java
+++ src/main/java/com/revature/training/ecommerce_project/Services/UserService.java
@@ -2,4 +2,51 @@ package com.revature.training.ecommerce_project.Services;
 
 public class UserService {
 
+    public void registerUser(String userData) {
+        
+    }
+
+    public void loginUder(String username, String password) {
+        
+    }
+
+    public void logoutUser(int userId) {
+        
+    }
+
+    public void getUserProfile(int userId) {
+        
+    }
+
+    public void updateUserProfile(int userId, String updatedData) {
+        
+    }
+
+    public void changePassword(int userId, String oldPassword, String newPassword) {
+        
+    }
+
+    public void generatePasswordResetToken(String email) {
+        
+    }
+
+    public void resetPassword(String token, String newPassword) {
+        
+    }
+
+    public void getUserOrders(int userId) {
+        
+    }
+
+    public void getSavedAddresses(int userId) {
+        
+    }
+
+    public void deleteAddress(int userId, int addressId) {
+        
+    }
+
+    public void getWishlist(int userId) {
+        
+    }
 }
