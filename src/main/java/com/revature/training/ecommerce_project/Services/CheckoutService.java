1c970450cc4775afd37a53c0e3b080e94b27c8d8
Aalok Zimmerman <azimmerman1245@gmail.com>
2025-10-08 14:06:37 -0500

    Added empty CheckoutService methods

--- src/main/java/com/revature/training/ecommerce_project/Services/CheckoutService.java
+++ src/main/java/com/revature/training/ecommerce_project/Services/CheckoutService.java
@@ -2,4 +2,51 @@ package com.revature.training.ecommerce_project.Services;
 
 public class CheckoutService {
     
+    public void validateOrder(String userID) {
+        
+    }
+
+    public void calculateTotal(String userID) {
+        
+    }
+
+    public void applyDiscountCode(String userID, String code) {
+        
+    }
+
+    public void removeDiscountCode(String userID) {
+        
+    }
+
+    public void setShippingMethod(String userID, String shippingMethod, String address) {
+        
+    }
+
+    public void setPaymentMethod(String userID, String paymentMethod) {
+        
+    }
+
+    public void calculateTaxes(String userID) {
+        
+    }
+
+    public void calculateShippingCost(String userID) {
+        
+    }
+
+    public void finalizeOrder(String userID, int checkoutDataID) {
+        
+    }
+
+    public void processPayment(String userID, int orderID, int paymentDataID) {
+        
+    }
+
+    public void sendOrderConfirmation(String userID, int orderID) {
+        
+    }
+
+    public void getCheckoutSummary(String userID) {
+        
+    }
 }
