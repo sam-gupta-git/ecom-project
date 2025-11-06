0a9f52e11b4c3cb0d4d07dd1a7d8eacb427f5419
sam-gupta-git <sam78748@gmail.com>
2025-11-05 18:02:51 -0600

    removed datainitialization code possibly causing API to crash

--- src/main/java/com/revature/training/ecommerce_project/config/DataInitializer.java
+++ src/main/java/com/revature/training/ecommerce_project/config/DataInitializer.java
@@ -1,31 +1,14 @@
 package com.revature.training.ecommerce_project.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-import com.revature.training.ecommerce_project.model.User;
-import com.revature.training.ecommerce_project.repository.UserRepository;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Component
 public class DataInitializer implements CommandLineRunner {
 
-    @Autowired
-    private UserRepository userRepository;
-    
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
     @Override
     public void run(String... args) throws Exception {
-        // Initialize test users if they don't exist
-        if (userRepository.findByUsername("testuser2") == null) {
-            User testUser = new User("testuser2", passwordEncoder.encode("password"), "testuser2@gmail.com");
-            userRepository.save(testUser);
-            System.out.println("Created test user: testuser2");
-        }
-        
-        // Sample item initialization removed - no longer auto-creating items
-        System.out.println("DataInitializer completed - no sample items created");
+        // DataInitializer setup complete - no test data creation
+        System.out.println("DataInitializer completed - no test users or items created");
     }
 }
